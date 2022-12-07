296c522515b9381872c155ca7c0cad77e2b323e7
Gerson Guerrero <jairguerrerosilvera@gmail.com>
2022-12-06 20:22:47 -0500

    add restaurants page

---
+++ pages/restaurants.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container restaurant">
+    <h1>Restaurants</h1>
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+import AppRestaurantInfo from '../components/AppRestaurantInfo.vue';
+export default {
+  components: {
+    AppRestaurantInfo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
